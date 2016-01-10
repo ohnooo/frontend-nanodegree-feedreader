a0523e59962bb3d29ba1e5bc3c852ebffc0f6840
ohnooo <eric.h.1688@gmail.com>
2016-01-10 01:32:22 -0800

    Comments
    Modify all comments to my own

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -9,17 +9,14 @@
  * to ensure they don't run until the DOM is ready.
  */
 $(function() {
-    /* This is our first test suite - a test suite just contains
-    * a related set of tests. This suite is all about the RSS
-    * feeds definitions, the allFeeds variable in our application.
-    */
+    /* Test Suite */
+
+    /* Test RSS Feeds */
     describe('RSS Feeds', function() {
-        /* This is our first test - it tests to make sure that the
-         * allFeeds variable has been defined and that it is not
-         * empty. Experiment with this before you get started on
-         * the rest of this project. What happens when you change
-         * allFeeds in app.js to be an empty array and refresh the
-         * page?
+        /* it <define spec> - Test allFeeds
+         * condition:
+         * - All to be defined
+         * - Not empty
          */
         it('are defined', function() {
             expect(allFeeds).toBeDefined();
@@ -27,9 +24,10 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
+        /* it <define spec> - Test has URL and NOT empty
+         * condition:
+         * Each item is defined
+         * Each item is not empty
          */
          it('allFeeds has URL and not empty', function(){
             for(var i = 0; i< allFeeds.length; i++ ){
@@ -38,9 +36,9 @@ $(function() {
             }
          });
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a name defined
-         * and that the name is not empty.
+        /* it <define spec> - loop each item
+         * Item.name is defined
+         * Item.name not empty
          */
          it('allFeeds has name and not empty', function(){
             for(var i = 0; i< allFeeds.length; i++ ){
@@ -51,26 +49,23 @@ $(function() {
     });
 
 
-    /* TODO: Write a new test suite named "The menu" */
+    /* Test for Menu */
     describe('The Menu', function(){
         // Jquery variable
         var $body = $('body'),
-            $menuIconLink = $('.menu-icon-link'),
+            $menuIconLink = $('.menu-icon-link');
 
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
+        /* it <define spec> - Menu hidden by default
+         * if <body> has class 'menu-hidden'
          */
          it('Menu should be hidden by default', function(){
             expect($body.hasClass('menu-hidden')).toEqual(true);
          });
 
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
+          /* it <define spec> - Menu change when click on icon
+           * $menuIconLink.trigger<fake trigger>
+           * http://api.jquery.com/trigger/
+           */
           it('Menu should toggle', function(){
             $menuIconLink.trigger('click');
             expect($body.hasClass('menu-hidden')).toEqual(false);
@@ -80,45 +75,53 @@ $(function() {
           });
     });
 
-    /* TODO: Write a new test suite named "Initial Entries" */
+    /* "Initial Entries" */
     describe('Initial Entries', function(){
-         /*TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test wil require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
-         */
+
+         /* Asynchronous test - beforeEach + done
+          * https://jasmine.github.io/2.3/introduction.html#section-Asynchronous_Support
+          */
          var id = 0;
          beforeEach(function(done){
-            loadFeed(id, function(){
-                done();
-            });
-         });
+            loadFeed(id, done);
+         });''
 
+         /* it <define spec> - Makes sure load is complete
+          * If complete number entry > 0
+          */
          it('Load is complete', function(){
             var numEntry = $(".entry-link").length;
             expect(numEntry).toBeGreaterThan(0);
          });
     });
-    /* TODO: Write a new test suite named "New Feed Selection"*/
+
+    /* "New Feed Selection"*/
     describe('New Feed Selection', function(){
-        /* TODO: Write a test that ensures when a new feed is loaded
-         * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.
-         */
+
+         /* Compare if new feeds is loaded content should change
+          * @{param} - content - initial data
+          * @{param} - newContent - new data
+          * beforeEach - Async load intial data
+          */
          var content, newContent;
          beforeEach(function(done){
             loadFeed(0, function(){
+                // save  feed[0] string to content
                 content = $('.feed').find('h2').text();
                 done();
             });
          });
 
+         /* it <define spec> - Ensure content change based on new feeds
+          * compare content : newContent -> not equal
+          */
          it('Content change to new feeds', function(done){
             loadFeed(1, function(){
+                // save feed[1] string to newContent
                 newContent = $('.feed').find('h2').text();
 
                 // expect inside of done to achieve asynchronous
+                // Test if content : newContent -> not equal -> if true === pass
                 expect(content).not.toEqual(newContent);
                 done();
             });
